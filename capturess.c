9e06912be6368d1580a2db64ec23724ea314aa58
Rafael Calleja <rafaelcalleja@gmail.com>
2018-12-26 16:52:41 +0100

    remove perror

--- capturess.c
+++ capturess.c
@@ -187,16 +187,16 @@ int main(int argc, char *argv[])
   // Creating socket file descriptor
   if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
   {
-    perror("socket failed");
-    exit(EXIT_FAILURE);
+    printf("socket failed");
+    return EXIT_FAILURE;
   }
 
   // Forcefully attaching socket to the port 8080
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                  &opt, sizeof(opt)))
   {
-    perror("setsockopt");
-    exit(EXIT_FAILURE);
+    printf("setsockopt");
+    return EXIT_FAILURE;
   }
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
@@ -206,14 +206,14 @@ int main(int argc, char *argv[])
   if (bind(server_fd, (struct sockaddr *)&address,
            sizeof(address)) < 0)
   {
-    perror("bind failed");
-    exit(EXIT_FAILURE);
+    printf("bind failed");
+    return EXIT_FAILURE;
   }
 
   if (listen(server_fd, 3) < 0)
   {
-    perror("listen");
-    exit(EXIT_FAILURE);
+    printf("socket failed");
+    return EXIT_FAILURE;
   }
 
   playHandle = livePlay(m_userId, &clientInfo, NULL, NULL);
@@ -231,8 +231,8 @@ int main(int argc, char *argv[])
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
                              (socklen_t *)&addrlen)) < 0)
     {
-      perror("accept");
-      exit(EXIT_FAILURE);
+      printf("accept");
+      return EXIT_FAILURE;
     }
 
     valread = read(new_socket, buffer, 1024);
@@ -245,7 +245,7 @@ int main(int argc, char *argv[])
 
     if (n < 0)
     {
-      perror("scandir");
+      printf("scandir");
       return EXIT_FAILURE;
     }
     else
